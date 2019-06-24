4ccf16966fff956dea44010100c8ff239c160418
AkiraFukushima <h3.poteto@gmail.com>
2019-06-24 09:16:58 +0900

    Upgrade postgresql version to 11.2

--- config/test.secret.exs
+++ config/test.secret.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :pleroma, Pleroma.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DB_USEr") || "pleroma",
+  username: System.get_env("DB_USER") || "pleroma",
   password: System.get_env("DB_PASSWORD") || "pleroma",
-  database: "pleroma_test",
+  database: System.get_env("DB_NAME") || "pleroma_test",
   hostname: System.get_env("DB_HOST") || "db",
   pool_size: 10
