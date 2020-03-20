880cfe5329b5876e7d655eb5744d4c3155983887
AkiraFukushima <h3.poteto@gmail.com>
2020-03-21 00:55:30 +0900

    Change event_source for MatchError in ActivityPub

--- lib/sentry_event_filter.ex
+++ lib/sentry_event_filter.ex
@@ -2,7 +2,7 @@ defmodule Pleroma.SentryEventFilter do
   @behaviour Sentry.EventFilter
 
   # https://docs.sentry.io/clients/elixir/#filtering-events
-  def exclude_exception?(%MatchError{}, :plug), do: true
+  def exclude_exception?(%MatchError{}, nil), do: true
 
   def exclude_exception?(%FunctionClauseError{}, :endpoint), do: true
 
