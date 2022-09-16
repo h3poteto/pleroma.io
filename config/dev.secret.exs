3ce853d0088ebb2f43660a07c64211524a66701c
AkiraFukushima <h3.poteto@gmail.com>
2022-09-16 20:39:41 +0900

    Replace Mix.Config to Config

--- config/dev.secret.exs
+++ config/dev.secret.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :pleroma, Pleroma.Web.Endpoint,
   http: [
@@ -13,14 +13,6 @@ config :pleroma, Pleroma.Web.Endpoint,
   watchers: [],
   secure_cookie_flag: false
 
-config :pleroma, Pleroma.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DB_USER"),
-  password: System.get_env("DB_PASSWORD"),
-  database: System.get_env("DB_NAME"),
-  hostname: System.get_env("DB_HOST"),
-  pool_size: 10
-
 config :pleroma, :instance,
   name: "Pleroma.io",
   email: "h3.poteto@gmail.com",
@@ -30,7 +22,7 @@ config :pleroma, :instance,
   dynamic_configuration: false,
   remote_post_retention_days: 365
 
-config :pleroma, :chat, enabled: false
+config :pleroma, :shout, enabled: false
 
 config :pleroma, :frontend_configurations,
   pleroma_fe: %{
--- config/test.secret.exs
+++ config/test.secret.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :pleroma, Pleroma.Repo,
   adapter: Ecto.Adapters.Postgres,
