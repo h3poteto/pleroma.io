2515c9143170ebfb4703260ea1d5c049a68a7b8c
AkiraFukushima <h3.poteto@gmail.com>
2022-09-16 20:52:52 +0900

    [from now] 2022/09/16 20:52:52
    diff --git a/config/test.secret.exs b/config/test.secret.exs
    new file mode 100644
    index 000000000..60abe5d5a
    --- /dev/null
    +++ b/config/test.secret.exs
    @@ -0,0 +1,9 @@
    +import Config
    +
    +config :pleroma, Pleroma.Repo,
    +  adapter: Ecto.Adapters.Postgres,
    +  username: System.get_env("DB_USER") || "pleroma",
    +  password: System.get_env("DB_PASSWORD") || "pleroma",
    +  database: System.get_env("DB_NAME") || "pleroma_test",
    +  hostname: System.get_env("DB_HOST") || "db",
    +  pool_size: 10

---
+++ config/test.secret.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :pleroma, Pleroma.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DB_USER") || "pleroma",
+  password: System.get_env("DB_PASSWORD") || "pleroma",
+  database: System.get_env("DB_NAME") || "pleroma_test",
+  hostname: System.get_env("DB_HOST") || "db",
+  pool_size: 10
